853c384412f82c762321e5736fb0a5cd5ce00ab1
ravitejajella <37808694+ravitejajella@users.noreply.github.com>
2025-07-01 11:54:24 -0300

    Delete Comps directory

--- Comps/comps/src/app/elements/times.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Times } from './times';
-
-describe('Times', () => {
-  it('should create an instance', () => {
-    const directive = new Times();
-    expect(directive).toBeTruthy();
-  });
-});
